6a18c88e87682cd16ed9bbc291e839277f51bd8d
droidsam1 <droidsam@gmail.com>
2022-09-17 19:20:26 +0200

    refactor: rename test class

--- src/test/java/com/droidsam/app/StatsCalculatorTest.java
+++ src/test/java/com/droidsam/app/StatsCalculatorTest.java
@@ -3,10 +3,10 @@ package com.droidsam.app;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class StatsCalculatorShould {
+public class StatsCalculatorTest {
 
     @Test
-    public void returnAnObjectWithStats() {
+    public void shouldReturnAnObjectWithStats() {
         Assertions.assertNotNull(StatsCalculator.calculate(new int[]{1, 2, 3}));
     }
 }
