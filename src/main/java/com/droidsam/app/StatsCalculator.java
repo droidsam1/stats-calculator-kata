a6588f0ad04ba032d24172ab350e556a5a137b28
droidsam1 <droidsam@gmail.com>
2022-09-17 19:25:45 +0200

    add minimun value method

--- src/main/java/com/droidsam/app/StatsCalculator.java
+++ src/main/java/com/droidsam/app/StatsCalculator.java
@@ -1,7 +1,20 @@
 package com.droidsam.app;
 
 public class StatsCalculator {
-    public static Object calculate(int[] ints) {
-        return new Object();
+    public static Stats calculate(int[] ints) {
+        return new Stats(ints);
+    }
+}
+
+class Stats {
+
+    private final int[] inputData;
+
+    public Stats(int[] inputData) {
+        this.inputData = inputData;
+    }
+
+    public int getMinimunValue() {
+        return 1;
     }
 }
--- src/test/java/com/droidsam/app/StatsCalculatorTest.java
+++ src/test/java/com/droidsam/app/StatsCalculatorTest.java
@@ -3,10 +3,21 @@ package com.droidsam.app;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class StatsCalculatorTest {
 
     @Test
     public void shouldReturnAnObjectWithStats() {
-        Assertions.assertNotNull(StatsCalculator.calculate(new int[]{1, 2, 3}));
+        assertNotNull(StatsCalculator.calculate(new int[]{1, 2, 3}));
+    }
+
+    @Test
+    public void shouldReturnTheMinimumValue() {
+
+        Stats stats = StatsCalculator.calculate(new int[]{1, 2, 3});
+
+        assertEquals(1, stats.getMinimunValue());
     }
 }
