d69fe5f417a652abf4537fd55f6dc985cd5882b2
droidsam1 <droidsam@gmail.com>
2022-09-18 10:18:03 +0200

    refactor: extract input examples to fixture class

---
+++ src/test/java/com/droidsam/app/SequenceFixture.java
@@ -0,0 +1,26 @@
+package com.droidsam.app;
+
+public class SequenceFixture {
+
+    private static final int[] UNSORTED_POSITIVE_ARRAY = {22, 7, 8, 10, 20, 3};
+    private static final int[] ARRAY_WITH_NEGATIVES = {2, 0, -1, -2};
+    private static final int[] ONLY_ONE_ELEMENT_ARRAY = {22};
+    private static final int[] SORTED_ARRAY = {1, 2, 3};
+
+
+    public static int[] getUnsortedPositiveArray() {
+        return UNSORTED_POSITIVE_ARRAY;
+    }
+
+    public static int[] getArrayWithNegatives() {
+        return ARRAY_WITH_NEGATIVES;
+    }
+
+    public static int[] getOnlyOneElementArray() {
+        return ONLY_ONE_ELEMENT_ARRAY;
+    }
+
+    public static int[] getSortedArray() {
+        return SORTED_ARRAY;
+    }
+}
--- src/test/java/com/droidsam/app/StatsCalculatorTest.java
+++ src/test/java/com/droidsam/app/StatsCalculatorTest.java
@@ -8,11 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StatsCalculatorTest {
 
-    public static final int[] UNSORTED_POSITIVE_ARRAY = {22, 7, 8, 10, 20, 3};
-    public static final int[] ARRAY_WITH_NEGATIVES = {2, 0, -1, -2};
-    public static final int[] ONLY_ONE_ELEMENT_ARRAY = {22};
-    public static final int[] SORTED_ARRAY = {1, 2, 3};
-
     @Test
     public void shouldReturnAnObjectWithStats() {
         assertNotNull(StatsCalculator.calculate(new int[]{1, 2, 3}));
@@ -31,7 +26,7 @@ public class StatsCalculatorTest {
     @Test
     public void shouldReturnTheMinimumValue() {
 
-        Stats stats = StatsCalculator.calculate(SORTED_ARRAY);
+        Stats stats = StatsCalculator.calculate(SequenceFixture.getSortedArray());
 
         assertEquals(1, stats.getMinimumValue());
     }
@@ -39,7 +34,7 @@ public class StatsCalculatorTest {
     @Test
     public void shouldReturnTheMinimumValueWhenArrayIsNotSorted() {
 
-        Stats stats = StatsCalculator.calculate(UNSORTED_POSITIVE_ARRAY);
+        Stats stats = StatsCalculator.calculate(SequenceFixture.getUnsortedPositiveArray());
 
         assertEquals(3, stats.getMinimumValue());
     }
@@ -48,7 +43,7 @@ public class StatsCalculatorTest {
     @Test
     public void shouldReturnTheMinimumValueWhenArrayIsOnlyOneElement() {
 
-        Stats stats = StatsCalculator.calculate(ONLY_ONE_ELEMENT_ARRAY);
+        Stats stats = StatsCalculator.calculate(SequenceFixture.getOnlyOneElementArray());
 
         assertEquals(22, stats.getMinimumValue());
     }
@@ -56,14 +51,14 @@ public class StatsCalculatorTest {
     @Test
     public void shouldReturnTheMinimumValueWhenArrayContainsNegativeNumbers() {
 
-        Stats stats = StatsCalculator.calculate(ARRAY_WITH_NEGATIVES);
+        Stats stats = StatsCalculator.calculate(SequenceFixture.getArrayWithNegatives());
 
         assertEquals(-2, stats.getMinimumValue());
     }
 
     @Test
-    public void shouldReturnTheMaximumValueWhenArrayIsOnlyOneElement(){
-        Stats stats = StatsCalculator.calculate(ONLY_ONE_ELEMENT_ARRAY);
+    public void shouldReturnTheMaximumValueWhenArrayIsOnlyOneElement() {
+        Stats stats = StatsCalculator.calculate(SequenceFixture.getOnlyOneElementArray());
 
         assertEquals(22, stats.getMaximumValue());
     }
@@ -71,7 +66,7 @@ public class StatsCalculatorTest {
     @Test
     public void shouldReturnTheMaximumValueWhenArrayIsNotSorted() {
 
-        Stats stats = StatsCalculator.calculate(UNSORTED_POSITIVE_ARRAY);
+        Stats stats = StatsCalculator.calculate(SequenceFixture.getUnsortedPositiveArray());
 
         assertEquals(22, stats.getMaximumValue());
     }
@@ -79,7 +74,7 @@ public class StatsCalculatorTest {
     @Test
     public void shouldReturnTheMaximumValueWhenArrayContainsNegativeNumbers() {
 
-        Stats stats = StatsCalculator.calculate(ARRAY_WITH_NEGATIVES);
+        Stats stats = StatsCalculator.calculate(SequenceFixture.getArrayWithNegatives());
 
         assertEquals(2, stats.getMaximumValue());
     }
@@ -87,7 +82,7 @@ public class StatsCalculatorTest {
     @Test
     public void shouldReturnTheMaximumValue() {
 
-        Stats stats = StatsCalculator.calculate(SORTED_ARRAY);
+        Stats stats = StatsCalculator.calculate(SequenceFixture.getSortedArray());
 
         assertEquals(3, stats.getMaximumValue());
     }
