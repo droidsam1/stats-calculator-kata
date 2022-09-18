987253455aa9615a60efe465aca6d3165d120db3
droidsam1 <droidsam@gmail.com>
2022-09-18 10:10:54 +0200

    refactor: extract common inputs to constants

--- src/test/java/com/droidsam/app/StatsCalculatorTest.java
+++ src/test/java/com/droidsam/app/StatsCalculatorTest.java
@@ -8,6 +8,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StatsCalculatorTest {
 
+    public static final int[] UNSORTED_POSITIVE_ARRAY = {22, 7, 8, 10, 20, 3};
+    public static final int[] ARRAY_WITH_NEGATIVES = {2, 0, -1, -2};
+    public static final int[] ONLY_ONE_ELEMENT_ARRAY = {22};
+    public static final int[] SORTED_ARRAY = {1, 2, 3};
+
     @Test
     public void shouldReturnAnObjectWithStats() {
         assertNotNull(StatsCalculator.calculate(new int[]{1, 2, 3}));
@@ -26,7 +31,7 @@ public class StatsCalculatorTest {
     @Test
     public void shouldReturnTheMinimumValue() {
 
-        Stats stats = StatsCalculator.calculate(new int[]{1, 2, 3});
+        Stats stats = StatsCalculator.calculate(SORTED_ARRAY);
 
         assertEquals(1, stats.getMinimumValue());
     }
@@ -34,7 +39,7 @@ public class StatsCalculatorTest {
     @Test
     public void shouldReturnTheMinimumValueWhenArrayIsNotSorted() {
 
-        Stats stats = StatsCalculator.calculate(new int[]{22, 7, 8, 10, 20, 3});
+        Stats stats = StatsCalculator.calculate(UNSORTED_POSITIVE_ARRAY);
 
         assertEquals(3, stats.getMinimumValue());
     }
@@ -43,7 +48,7 @@ public class StatsCalculatorTest {
     @Test
     public void shouldReturnTheMinimumValueWhenArrayIsOnlyOneElement() {
 
-        Stats stats = StatsCalculator.calculate(new int[]{22});
+        Stats stats = StatsCalculator.calculate(ONLY_ONE_ELEMENT_ARRAY);
 
         assertEquals(22, stats.getMinimumValue());
     }
@@ -51,22 +56,22 @@ public class StatsCalculatorTest {
     @Test
     public void shouldReturnTheMinimumValueWhenArrayContainsNegativeNumbers() {
 
-        Stats stats = StatsCalculator.calculate(new int[]{2, 0, -1, -2});
+        Stats stats = StatsCalculator.calculate(ARRAY_WITH_NEGATIVES);
 
         assertEquals(-2, stats.getMinimumValue());
     }
 
     @Test
     public void shouldReturnTheMaximumValueWhenArrayIsOnlyOneElement(){
-        Stats stats = StatsCalculator.calculate(new int[]{2, 0});
+        Stats stats = StatsCalculator.calculate(ONLY_ONE_ELEMENT_ARRAY);
 
-        assertEquals(2, stats.getMaximumValue());
+        assertEquals(22, stats.getMaximumValue());
     }
 
     @Test
     public void shouldReturnTheMaximumValueWhenArrayIsNotSorted() {
 
-        Stats stats = StatsCalculator.calculate(new int[]{22, 7, 8, 10, 20, 3});
+        Stats stats = StatsCalculator.calculate(UNSORTED_POSITIVE_ARRAY);
 
         assertEquals(22, stats.getMaximumValue());
     }
@@ -74,7 +79,7 @@ public class StatsCalculatorTest {
     @Test
     public void shouldReturnTheMaximumValueWhenArrayContainsNegativeNumbers() {
 
-        Stats stats = StatsCalculator.calculate(new int[]{2, 0, -1, -2});
+        Stats stats = StatsCalculator.calculate(ARRAY_WITH_NEGATIVES);
 
         assertEquals(2, stats.getMaximumValue());
     }
